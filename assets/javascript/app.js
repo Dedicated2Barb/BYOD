51946f0e3567c415cefe64a7dc1cb525ff4ffdf4
Joshua Gomez <joshuagomez@Joshuas-MacBook-Pro.local>
2017-11-09 19:42:04 -0500

    updated html and js

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(number);
+
+
+$(".birth-container").on("keypress", function(enteredNo) {
+	if (enteredNo === number[i]) {
+		console.log(number[i]);
+	$(".birth-container").html(enteredNo);
+
+}
+});
+ 
